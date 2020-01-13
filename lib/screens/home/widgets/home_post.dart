efa0c9d45e883207e338f71a68ca50ad42bbea5c
Radu Denis Florian <dfr@heimdalsecurity.com>
2020-01-13 09:33:04 +0200

    Move share button, add date added for post text, add background color for post header row, not FUNCTIONAL post menu popup

--- lib/screens/home/widgets/home_post.dart
+++ lib/screens/home/widgets/home_post.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:goalsfrontend/models/home/post.dart';
 import 'package:goalsfrontend/utils/common/helpers/build_context_extensions.dart';
 
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
+
 class HomePost extends StatefulWidget {
   final Post post;
 
@@ -17,14 +19,25 @@ class _HomePostState extends State<HomePost> {
     widget.post.isLiked = !widget.post.isLiked;
   }
 
+  Widget onPostMenuPressed(int postId) {
+    return new PopupMenuButton(
+        key: new GlobalKey(),
+        itemBuilder: (_) => <PopupMenuItem<String>>[
+              new PopupMenuItem<String>(
+                  child: const Text('Doge'), value: 'Doge'),
+              new PopupMenuItem<String>(
+                  child: const Text('Lion'), value: 'Lion'),
+            ],
+        onSelected: (_) {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Row(
+      Container(
+        color: Colors.amber,
+          child: Row(
         children: <Widget>[
-          Padding(
-              padding:
-                  new EdgeInsets.only(top: context.heightFromPercentage(10))),
           Padding(
               child: CircleAvatar(
                   backgroundImage: NetworkImage(widget.post.userAvatarUrl)),
@@ -40,12 +53,16 @@ class _HomePostState extends State<HomePost> {
             ],
           ),
           Spacer(),
+          GestureDetector(
+            child: Icon(Icons.share, color: Colors.black),
+            onTap: () {},
+          ),
           IconButton(
             icon: Icon(Icons.more_vert),
-            onPressed: () {},
-          )
+            onPressed: () => onPostMenuPressed(widget.post.id),
+          ),
         ],
-      ),
+      )),
       Row(
         children: <Widget>[
           Column(
@@ -53,7 +70,7 @@ class _HomePostState extends State<HomePost> {
               Row(
                 children: <Widget>[
                   Image.network(widget.post.contentUrl,
-                      fit: BoxFit.fitWidth,
+                      fit: BoxFit.fill,
                       width: context.width(),
                       height: context.heightFromPercentage(30))
                 ],
@@ -74,25 +91,23 @@ class _HomePostState extends State<HomePost> {
                 Text("20 likes", style: TextStyle(fontStyle: FontStyle.normal)),
                 Text("5 comments",
                     style: TextStyle(fontStyle: FontStyle.italic)),
+                Text("2 days ago",
+                    style: TextStyle(fontStyle: FontStyle.italic))
               ],
             ),
           ),
           Spacer(),
-          GestureDetector(
-            child: widget.post.isLiked == false
-                ? Icon(Icons.favorite_border, color: Colors.black)
-                : Icon(Icons.favorite, color: Colors.red),
-            onTap: () => setState(() => onFavoritePressed()),
-            
-          ),
           Padding(
-              padding: EdgeInsets.only(right: context.widthFromPercentage(4)),
-              child: GestureDetector(
-                child: Icon(Icons.share, color: Colors.black),
-                onTap: () {},
-              ))
+              padding: EdgeInsets.only(right: context.widthFromPercentage(3)),
+              child: IconButton(
+                icon: widget.post.isLiked == false
+                    ? Icon(Icons.favorite_border, color: Colors.black)
+                    : Icon(Icons.favorite, color: Colors.red),
+                onPressed: () => setState(() => onFavoritePressed()),
+              )),
         ],
-      )
+      ),
+      Padding(padding: EdgeInsets.only(bottom: context.heightFromPercentage(2)))
     ]);
   }
 }
